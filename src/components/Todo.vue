cc92b5102e06f56bad7481980caa6a2a4bb6dcd0
Sopanvil <lenivkov12@gmail.com>
2020-03-26 00:03:16 +0300

    AddButton

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -2,11 +2,18 @@
   <div class="container">
     <div class="row">
       <div class="tasks-title animated fadeInLeft">
-        <p>Ваши Задачи</p>
+        <p>Ваши Задачи {{newTask}}</p>
       </div>
     </div>
     <div class="row">
-      <div v-for="(t, index) in tasks" :key="index" class="tasks col-md-12 animated fadeInRight">
+      <div class="tasks-input offset-md-2 col-md-7">
+        <input class="tasks-input-text" type="text" v-model="newTask" placeholder="Создайте вашу задачу">
+        <input class="tasks-input-button" type="button" value="+">
+      </div>
+
+    </div>
+    <div class="row">
+      <div v-for="(t, index) in tasks" :key="index" class="tasks col animated fadeInRight">
         {{t.title}}
       </div>
     </div>
@@ -32,7 +39,8 @@ export default {
             }
           ]
         }
-      ]
+      ],
+      newTask: ''
     };
   }
 };
@@ -47,5 +55,13 @@ export default {
   font-weight: 700;
   font-size: 75px;
   color: #fff;
+}
+.tasks-input-text{
+  padding: 10px 100px 10px 100px;
+  margin-bottom: 25px;
+}
+.tasks-input-button{
+  padding: 10px 100px 10px 100px;
+  margin-bottom: 25px;
 }
 </style>
